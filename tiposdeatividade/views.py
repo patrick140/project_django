ffc2f631f058ffd7080ed254cd8b8b0656581701
patrick140 <96891037+patrick140@users.noreply.github.com>
2025-05-16 21:21:45 -0300

    Commit final dia 16.05.25

--- tiposdeatividade/views.py
+++ tiposdeatividade/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Olá! Eu sou o index.")
+
+def exibe_mensagem(request):
+    t_html = '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> </head> <body> </body> </html>'
+    return HttpResponse(t_html)

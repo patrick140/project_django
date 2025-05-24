7bf2478f0bd4d74bb188727f8bad63762c489fc6
patrick140 <96891037+patrick140@users.noreply.github.com>
2025-05-23 21:31:05 -0300

    Commit final do dia 05

--- setup/settings.py
+++ setup/settings.py
@@ -125,10 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_DIRS = [
+STATICFILES_DIRS = [
     BASE_DIR / STATIC_URL, 
 ]
-STATIC_ROOT=os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
--- setup/urls.py
+++ setup/urls.py
@@ -17,8 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('', TemplateView.as_view(template_name='escola.html')),
+
     path('', 
          include("tiposdeatividade.urls") ),
     path('', 
